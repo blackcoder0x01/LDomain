c38480e35ed104ba1b5d7f877e169fa2efb06d06
Akash Sarkar <46501627+Akash0x01@users.noreply.github.com>
2020-05-09 16:34:40 +0600

    Update domainresolver.sh

--- domainresolver.sh
+++ domainresolver.sh
@@ -4,14 +4,14 @@ if [ ! "$1" ]; then
 	echo "Please Used ./domainresolve.sh weblist.txt output-folder-location"
 	exit 1
 fi
-cat $1 | sort -u | while read line; do
+cat "$1" | sort -u | while read line; do
     if [ $(curl --write-out %{http_code} --silent --output /dev/null -m 5 $line) = 000 ]
     then
       echo "$line was Error"
-      touch $2/ErrorWebsite.html
-      echo "<b><a href="$line">$line</b></a> Error Website</br>" >> $2/ErrorWebsite.html
+      touch $2ErrorWebsite.html
+      echo "<b><a href="$line">$line</b></a> Error Website</br>" >> $2ErrorWebsite.html
     else
       echo "$line is up"
-      echo "$line" >> $2/Live.$1
+      echo $line >> $2livesite.txt
     fi
 done
