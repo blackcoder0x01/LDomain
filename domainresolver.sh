1e3dfe70ff66a1d3f387ac1889d767830d405fb1
Akash Sarkar <46501627+Akash0x01@users.noreply.github.com>
2020-05-30 22:49:41 +0600

    Update domainresolver.sh

--- domainresolver.sh
+++ domainresolver.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+CYAN='\033[0;36m'
+RED='\033[0;31m'
+NC='\033[0m'
+SECONDS=0
+
 if [ ! "$1" ]; then
 	echo "Missing parameter"
 	echo "Please Used ./domainresolve.sh weblist.txt output-folder-location"
@@ -7,11 +13,15 @@ fi
 cat "$1" | sort -u | while read line; do
     if [ $(curl --write-out %{http_code} --silent --output /dev/null -m 5 $line) = 000 ]
     then
-      echo "$line was Error"
+      echo -e "$line ${RED}is Error/404${NC}"
       touch $2ErrorWebsite.html
-      echo "<b><a href="$line">$line</b></a> Error Website</br>" >> $2ErrorWebsite.html
+      echo "<b><a href="https://$line">$line</b></a> Error Website</br>" >> $2ErrorWebsite.html
     else
-      echo "$line is up"
+      echo -e "$line ${CYAN}is Live${NC}"
       echo $line >> $2livesite.txt
     fi
 done
+echo -e ""
+echo -e "${RED}Scan finished successfully${NC}"
+duration=$SECONDS
+echo "Scan completed in : $(($duration / 60)) minutes and $(($duration % 60)) seconds."
